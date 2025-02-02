628cd8d5b27b966003b4fde1515efbb85c4b47b1
Fufuffu <victor800000@gmail.com>
2025-02-02 18:40:20 +0100

    Pinned particles

--- app/Main.hs
+++ app/Main.hs
@@ -16,7 +16,6 @@ import Raylib.Util.Math
 data Particle = Particle
   { currentPos :: Vector2,
     lastPos :: Vector2,
-    initialPos :: Vector2,
     isFixed :: Bool
   }
   deriving (Show, Eq)
@@ -38,12 +37,18 @@ data AppState = AppState
     constraints :: V.Vector Constraint
   }
 
+gravity :: Vector2
+gravity = Vector2 0 98
+
+particleSpacing :: Int
+particleSpacing = 20
+
 startup :: IO AppState
 startup = do
   window <- initWindow 800 600 "Verlet integration"
   setTargetFPS 60
 
-  let cloth = createCloth 600 400 20 100 10
+  let cloth = createCloth 600 400 particleSpacing 100 10
   return
     ( AppState
         { resources = window,
@@ -70,16 +75,18 @@ createParticle spacing startX startY width idx = do
   let (y, x) = divMod idx (width + 1)
   let pos = Vector2 (fromIntegral (startX + x * spacing)) (fromIntegral (startY + y * spacing))
 
-  Particle {initialPos = pos, currentPos = pos, lastPos = pos, isFixed = y == 0}
+  Particle {currentPos = pos, lastPos = pos, isFixed = y == 0}
 
 mainLoop :: AppState -> IO AppState
 mainLoop state = do
+  -- Since we do not run in background, dragging the window will break the simulation
+  -- due to a huge delta time, this is ok for now :)
   dt <- getFrameTime
   drawing
     ( do
         clearBackground rayWhite
 
-        mapM_ (\(Particle pos _ _ _) -> drawCircleV pos 5 red) (particles state)
+        mapM_ (\(Particle pos _ _) -> drawCircleV pos 5 red) (particles state)
         mapM_ (drawConstraint (particles state)) (constraints state)
     )
     >> return
@@ -87,23 +94,17 @@ mainLoop state = do
         { particles = applyConstraints (V.map (updateParticle dt) (particles state)) (constraints state)
         }
 
-drawConstraint :: V.Vector Particle -> Constraint -> IO ()
-drawConstraint particles (Constraint idx1 idx2) = do
-  let p1 = particles V.! idx1
-  let p2 = particles V.! idx2
-
-  drawLineV (currentPos p1) (currentPos p2) lightGray
-
 updateParticle :: Float -> Particle -> Particle
-updateParticle dt particle = do
-  let acceleration = Vector2 0 98
+updateParticle dt particle =
+  if isFixed particle
+    then particle
+    else do
+      let nextPosition = (currentPos particle |* 2) |-| lastPos particle |+| (gravity |* (dt * dt))
 
-  let nextPosition = (currentPos particle |* 2) |-| lastPos particle |+| (acceleration |* (dt * dt))
-
-  particle
-    { currentPos = vectorMin nextPosition (Vector2 800 600),
-      lastPos = currentPos particle
-    }
+      particle
+        { currentPos = vectorMin nextPosition (Vector2 800 600),
+          lastPos = currentPos particle
+        }
 
 applyConstraints :: V.Vector Particle -> V.Vector Constraint -> V.Vector Particle
 applyConstraints particles constraints =
@@ -114,17 +115,29 @@ applyConstraint particles (Constraint idx1 idx2) = do
   let p1 = particles V.! idx1
   let p2 = particles V.! idx2
 
-  let spacing = 20
-
   let diff = currentPos p1 |-| currentPos p2
   let currentLength = magnitude diff
-  let diffFactor = (spacing - currentLength) / currentLength * 0.5
+  let diffFactor = ((fromIntegral particleSpacing) - currentLength) / currentLength * 0.5
   let offset = diff |* diffFactor
 
-  let newP1 = p1 {currentPos = currentPos p1 |+| offset}
-  let newP2 = p2 {currentPos = currentPos p2 |-| offset}
+  let newP1 =
+        if not (isFixed p1)
+          then p1 {currentPos = currentPos p1 |+| offset}
+          else p1
+
+  let newP2 =
+        if not (isFixed p2)
+          then p2 {currentPos = currentPos p2 |-| offset}
+          else p2
 
   particles V.// [(idx1, newP1), (idx2, newP2)]
+
+drawConstraint :: V.Vector Particle -> Constraint -> IO ()
+drawConstraint particles (Constraint idx1 idx2) = do
+  let p1 = particles V.! idx1
+  let p2 = particles V.! idx2
+
+  drawLineV (currentPos p1) (currentPos p2) lightGray
 
 shouldClose :: AppState -> IO Bool
 shouldClose _ = windowShouldClose
